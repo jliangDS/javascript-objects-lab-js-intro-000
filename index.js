3a1f1c456bba238ad41f623af3aa4ee35f7fbae5
Jun Liang <illuminatemynight@gmail.com>
2019-06-18 19:09:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ var updateObjectWithKeyAndValue = function(object, key, value){
 var destructivelyUpdateObjectWithKeyAndValue = function(object, key, value){
   return Object.assign(object, {[key]: value})
 }
+
+var deleteFromObjectByKey = function(object, key){
+  
+}
+
+var destructivelyDeleteFromObjectByKey = function(object, key){
+  delete object[key]
+  return object
+}
