405cea2179a6959bf5a384655701514d91df9835
Jun Liang <illuminatemynight@gmail.com>
2019-06-18 19:05:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var recipes = {};
 var updateObjectWithKeyAndValue = function(object, key, value){
   return Object.assign({}, object, {[key]: value})
 }
+
+var destructivelyUpdateObjectWithKeyAndValue = function(object, key, value){
+  return Object.assign({}, object, {[key]: value})
+}
