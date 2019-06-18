de7bfac3b0e963ae9633d9196e3b2c835b6de5c9
Jun Liang <illuminatemynight@gmail.com>
2019-06-18 19:05:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var updateObjectWithKeyAndValue = function(object, key, value){
 }
 
 var destructivelyUpdateObjectWithKeyAndValue = function(object, key, value){
-  return Object.assign({}, object, {[key]: value})
+  return Object.assign(object, {[key]: value})
 }
